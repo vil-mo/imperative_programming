214b409a65dc9bab4d50f24630fec1c9ce222cb4
vil'mo <morgraphvillain@gmail.com>
2023-09-10 18:41:56 +0700

    set1 (no9)

---
+++ set2/task3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    double negative = 0;
+    double zero = 0;
+    double positive = 0;
+
+    
+
+    printf ( "%0.5lf %0.5ln %0.5ln\n" , negative, zero, positive);
+
+    return 0;
+}
